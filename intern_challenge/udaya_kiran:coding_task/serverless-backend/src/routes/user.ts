4750437cc7ee22e4d575fc73e56a993b6b438ebc
UdayaKiran442 <gudaya2002@gmail.com>
2024-02-15 17:17:27 +0530

    changed route logic

--- intern_challenge/udaya_kiran:coding_task/serverless-backend/src/routes/user.ts
+++ intern_challenge/udaya_kiran:coding_task/serverless-backend/src/routes/user.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import { RegExpRouter } from "hono/router/reg-exp-router";
+import db from "../config/database";
+import { userSchema } from "../../db/schema";
 
 const userRoute = new Hono({ router: new RegExpRouter() });
 
-userRoute.get("/new-user", async (c) => {
-  return c.text("This route will create new user");
+userRoute.get("/getUsers", async (c) => {
+  const res = await db.select().from(userSchema);
+  return c.json(res);
 });
 
 export { userRoute };
