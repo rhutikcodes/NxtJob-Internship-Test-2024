5d1c0f716fc08f4cb4bda60d99ae765b475f9e71
UdayaKiran442 <gudaya2002@gmail.com>
2024-02-16 10:57:13 +0530

    route to add new user

--- intern_challenge/udaya_kiran:coding_task/serverless-backend/src/index.ts
+++ intern_challenge/udaya_kiran:coding_task/serverless-backend/src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
 import { RegExpRouter } from "hono/router/reg-exp-router";
+import { prettyJSON } from "hono/pretty-json";
 
 import { userRoute } from "./routes/user";
 
 const app = new Hono({ router: new RegExpRouter() });
+
+app.use(prettyJSON());
+
+app.use("*", async (c, next) => {
+  await next();
+  if (c.error) {
+    c.json(c.error);
+  }
+});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
--- intern_challenge/udaya_kiran:coding_task/serverless-backend/src/routes/user.ts
+++ intern_challenge/udaya_kiran:coding_task/serverless-backend/src/routes/user.ts
@@ -1,13 +1,25 @@
 import { Hono } from "hono";
 import { RegExpRouter } from "hono/router/reg-exp-router";
-import db from "../config/database";
+
 import { userSchema } from "../../db/schema";
+
+import db from "../config/database";
 
 const userRoute = new Hono({ router: new RegExpRouter() });
 
 userRoute.get("/getUsers", async (c) => {
   const res = await db.select().from(userSchema);
   return c.json(res);
+});
+
+userRoute.post("/new-user", async (c) => {
+  try {
+    const body = await c.req.json();
+    const newUser = await db.insert(userSchema).values(body);
+    return c.json(newUser);
+  } catch (error) {
+    return c.json(error);
+  }
 });
 
 export { userRoute };
