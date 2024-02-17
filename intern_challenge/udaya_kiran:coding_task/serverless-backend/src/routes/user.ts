0909b8c3baa73d39328a938db333e8384eff613e
UdayaKiran442 <gudaya2002@gmail.com>
2024-02-17 11:13:48 +0530

    user login

--- intern_challenge/udaya_kiran:coding_task/serverless-backend/src/routes/user.ts
+++ intern_challenge/udaya_kiran:coding_task/serverless-backend/src/routes/user.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { RegExpRouter } from "hono/router/reg-exp-router";
+import { eq } from "drizzle-orm";
+import { sign } from "@tsndr/cloudflare-worker-jwt";
 
 import { userSchema } from "../../db/schema";
 
@@ -20,6 +22,27 @@ userRoute.post("/new-user", async (c) => {
   } catch (error) {
     return c.json(error);
   }
+});
+
+userRoute.post("/login", async (c) => {
+  const body = await c.req.json();
+  const user = await db
+    .select()
+    .from(userSchema)
+    .where(eq(userSchema.email, body.email));
+  if (user.length == 0)
+    return c.json({ success: false, error: "Invalid email id" });
+  if (user[0].password !== body.password)
+    return c.json({ success: false, error: "Invalid Password" });
+  const token = await sign(
+    { ...user, exp: Math.floor(Date.now() / 1000) + 24 * (60 * 60) },
+    "fjwkn"
+  );
+  return c.json({
+    success: true,
+    message: "Login successful",
+    token,
+  });
 });
 
 export { userRoute };
