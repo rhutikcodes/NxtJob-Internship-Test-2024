6eb0ddebea57fd5e53df11a45a55e75fab0811f1
Suyash Srivastava <suyashsrivastava5053@gmail.com>
2024-02-22 23:21:52 +0530

    Useless Code Cleanup

--- coding_task/calendly_clone/index.ts
+++
@@ -1,5 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
-export default db;
